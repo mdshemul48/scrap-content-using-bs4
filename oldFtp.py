1670ecb062fd2b4e8f4a701e2de74ccaaff2321c
MD. Shimul <mdshemul480@gmail.com>
2023-01-09 17:02:44 +0600

    adding old ftp url

--- oldFtp.py
+++ oldFtp.py
@@ -6,26 +6,33 @@ class Ftp():
         self.page = page
         return self
 
+    def __getContent(self, oldContent: str) -> list[dict]:
+        print(oldContent)
+
     def fetchData(self):
         data = requests.get(f"http://circleftp.net/new-post-api/page/{self.page}").json()
         postIds: list[str] = data.keys()
         posts = []
         for postId in postIds:
-            tags, categories,  title, poster, downloadLink, content = data[postId]
-
+            print(data[postId])
+            tags, categories,  title, poster, downloadLink, content, url = data[postId]
+            self.__getContent(content)
             post = {
+                "id": postId,
                 "title": title,
                 "tags": tags,
                 "poster": poster,
                 "categories": categories.split(",")[:-1],
                 "downloadLink": downloadLink,
-                "content": content
+                "content": content,
+                "url": url
             }
             posts.append(post)
+            break
         return posts
 
 
 if __name__ == '__main__':
     ftp = Ftp()
-    data = ftp.page(1).fetchData()
-    print(data)
+    data = ftp.page(2).fetchData()
+    # print(data)
